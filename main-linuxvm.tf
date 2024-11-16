d3292af8e937228df88a72d521a314499d98d280
ubuntu <kvchandra2024@gmail.com>
2024-11-16 21:28:47 +0530

    modified the tf file

--- main-linuxvm.tf
+++ main-linuxvm.tf
@@ -3,21 +3,40 @@ resource "azurerm_resource_group" "rgabc" {
   name     = var.rg_name
 }
 
-resource "azurerm_virtual_network" "vnetabc" {
-  name                = var.vnet_name
+resource "azurerm_virtual_network" "vnetabc1" {
+  name                = "vnet1"
   resource_group_name = var.rg_name
   address_space       = var.address_space1
   location            = var.location
 }
 
+resource "azurerm_virtual_network" "network2" {
+  name                = "vnet2"
+  resource_group_name = var.rg_name
+  address_space       = var.address_space1
+  location            = var.location
+}
+
+
 resource "azurerm_subnet" "subnetabc" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rgabc.name
-  virtual_network_name = azurerm_virtual_network.vnetabc.name
+  virtual_network_name = azurerm_virtual_network.vnetabc1.name
   address_prefixes     = var.subnet_space1
 
 	depends_on = [azurerm_resource_group.rgabc]
 }
+
+
+resource "azurerm_subnet" "subnetabc2" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rgabc.name
+  virtual_network_name = azurerm_virtual_network.network2.name
+  address_prefixes     = var.subnet_space2
+
+        depends_on = [azurerm_resource_group.rgabc]
+}
+
 
 resource "azurerm_ssh_public_key" "ssh" {
   name                = var.key
@@ -36,7 +55,7 @@ resource "azurerm_public_ip" "public-ipabc" {
 
 
 resource "azurerm_network_interface" "nicabc" {
-  name                = var.nic_name
+  name                = "vmnic1"
   resource_group_name = azurerm_resource_group.rgabc.name
   location            = var.location
   ip_configuration {
@@ -45,6 +64,22 @@ resource "azurerm_network_interface" "nicabc" {
     public_ip_address_id          = azurerm_public_ip.public-ipabc.id
     subnet_id                     = azurerm_subnet.subnetabc.id
   }
+
+resource "azurerm_network_interface" "nic_vm2" {
+  name                = "nic-vm2"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rgabc.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetabc2.id
+    private_ip_address            = var.private_ip
+    private_ip_address_allocation = "Static"
+  }
+}
+
+
+
 }
 resource "azurerm_linux_virtual_machine" "linux-vm1" {
   name                = var.vm_name
