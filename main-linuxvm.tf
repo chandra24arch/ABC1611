ac069e6c1a9fd45f72b20cdafb484f3cfdfd5942
ubuntu <kvchandra2024@gmail.com>
2024-11-16 20:33:24 +0530

    tf files to abc

---
+++ main-linuxvm.tf
@@ -0,0 +1,108 @@
+resource "azurerm_resource_group" "rgabc" {
+  location = var.location
+  name     = var.rg_name
+}
+
+resource "azurerm_virtual_network" "vnetabc" {
+  name                = var.vnet_name
+  resource_group_name = var.rg_name
+  address_space       = var.address_space1
+  location            = var.location
+}
+
+resource "azurerm_subnet" "subnetabc" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rgabc.name
+  virtual_network_name = azurerm_virtual_network.vnetabc.name
+  address_prefixes     = var.subnet_space1
+
+	depends_on = [azurerm_resource_group.rgabc]
+}
+
+resource "azurerm_ssh_public_key" "ssh" {
+  name                = var.key
+  resource_group_name = azurerm_resource_group.rgabc.name
+  location            = var.location
+  public_key          = file("~/.ssh/id_rsa.pub")
+}
+
+resource "azurerm_public_ip" "public-ipabc" {
+  name                = var.public-ip_name
+  resource_group_name = azurerm_resource_group.rgabc.name
+  location            = var.location
+  allocation_method   = "Static"
+}
+
+
+
+resource "azurerm_network_interface" "nicabc" {
+  name                = var.nic_name
+  resource_group_name = azurerm_resource_group.rgabc.name
+  location            = var.location
+  ip_configuration {
+    name                          = var.ip_name
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public-ipabc.id
+    subnet_id                     = azurerm_subnet.subnetabc.id
+  }
+}
+resource "azurerm_linux_virtual_machine" "linux-vm1" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rgabc.name
+  location            = var.location
+
+  admin_username        = var.admin
+  network_interface_ids = [azurerm_network_interface.nicabc.id]
+  size                  = var.size
+  
+ admin_ssh_key {
+    
+    public_key = file("~/.ssh/id_rsa.pub")
+    username   = var.admin
+}
+  
+ os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+}
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg
+  location            = azurerm_resource_group.rgabc.location
+  resource_group_name = azurerm_resource_group.rgabc.name
+
+  security_rule {
+    name                       = "sshport"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+security_rule {
+    name                       = "icmp"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+
+}
+}
+resource "azurerm_network_interface_security_group_association" "nsgasoc" {
+  network_interface_id      = azurerm_network_interface.nicabc.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
---
+++ variable-lin.tf
@@ -0,0 +1,75 @@
+variable "rg_name" {
+  default = "rg_abc"
+}
+
+variable "location" {
+  default = "EastAsia"
+}
+
+variable "address_space1" {
+  default = ["10.5.0.0/16"]
+  type    = list(string)
+}
+
+variable "address_space2" {
+  default = ["10.15.0.0/16"]
+  type    = list(string)
+}
+
+
+
+variable "vnet_name" {
+  default = "abc_vnet"
+}
+variable "subnet_space1" {
+  default = ["10.5.1.0/24", "10.5.2.0/24"]
+  type    = list(string)
+}
+
+variable "subnet_space2" {
+  default = ["10.15.0.0/24", "10.15.2.0/24"]
+  type    = list(string)
+}
+
+
+variable "subnet_name" {
+  default = "abc_subnet"
+}
+
+variable "key" {
+  description = "abc SSH public key"
+  type        = string
+  default = "abcsshkey"
+}
+variable "nic_name" {
+  default = "abcnic"
+}
+variable "public-ip_name" {
+  default = "public-winip"
+}
+
+variable "ip_name" {
+  default = "ip"
+}
+variable "sku" {
+  default = "22.04-LTS"
+}
+variable "vm_name" {
+  default = "linuxtfvm"
+}
+variable "admin" {
+  default = "azureadmin"
+}
+variable "size" {
+  default = "Standard_B1s"
+}
+variable "user" {
+  default = "azureuser"
+}
+
+
+
+variable "nsg" {
+  default = "nsglinux"
+}
+
